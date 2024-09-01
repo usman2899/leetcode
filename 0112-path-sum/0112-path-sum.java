539e64e458583f0a2a31de29c02eed7b3fe4ef49
Usman Bin Arshad <hoxiusman@gmail.com>
2024-09-01 22:03:41 +0500

    Time: 0 ms (100.00%), Space: 42.8 MB (90.34%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,29 +15,43 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        
         if(root == null) {
             return false;
         }
-        Stack<TreeNode> nodeStack = new Stack<>();
-        Stack<Integer> numStack = new Stack<>();
-        nodeStack.push(root);
-        numStack.push(targetSum - root.val);
         
-        while (!nodeStack.isEmpty()) {
-            TreeNode currNode = nodeStack.pop();
-            int currSum = numStack.pop();
-            if (currNode.left == null && currNode.right == null && currSum == 0) {
-                return true;
-            }
-            if (currNode.left != null) {
-                nodeStack.push(currNode.left);
-                numStack.push(currSum - currNode.left.val);
-            }
-            if (currNode.right != null) {
-                nodeStack.push(currNode.right);
-                numStack.push(currSum - currNode.right.val);
-            }
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
         }
-        return false;
+        
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        
+//         //Solution 1 (poor time comp)
+//         if(root == null) {
+//             return false;
+//         }
+//         Stack<TreeNode> nodeStack = new Stack<>();
+//         Stack<Integer> numStack = new Stack<>();
+//         nodeStack.push(root);
+//         numStack.push(targetSum - root.val);
+        
+//         while (!nodeStack.isEmpty()) {
+//             TreeNode currNode = nodeStack.pop();
+//             int currSum = numStack.pop();
+//             if (currNode.left == null && currNode.right == null && currSum == 0) {
+//                 return true;
+//             }
+//             if (currNode.left != null) {
+//                 nodeStack.push(currNode.left);
+//                 numStack.push(currSum - currNode.left.val);
+//             }
+//             if (currNode.right != null) {
+//                 nodeStack.push(currNode.right);
+//                 numStack.push(currSum - currNode.right.val);
+//             }
+//         }
+//         return false;
+        
+        
     }
 }
