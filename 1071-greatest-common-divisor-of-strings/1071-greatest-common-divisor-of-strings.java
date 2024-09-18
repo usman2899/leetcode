1ae851f20bdea725b684624fc6c7a64bab145486
Usman Bin Arshad <hoxiusman@gmail.com>
2024-09-18 17:50:34 +0500

    Time: 1 ms (69.88%), Space: 41.9 MB (89.64%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -4,49 +4,66 @@ class Solution {
         if (!(str1 + str2).equals(str2 + str1)) {
             return "";
         }
-        int len1 = str1.length();
-        int len2 = str2.length();
-        String larger = "";
-        String smaller = "";
-        
-        if (len1 == len2) {
-            if (str1.equals(str2)) {
-                return str1;
-            } else {
-                return "";
-            }
-        } else if (len1 > len2) {
-            larger = str1;
-            smaller = str2;
-        } else {
-            larger = str2;
-            smaller = str1;
+
+        int gcdLength = gcd(str1.length(), str2.length());
+
+        return str1.substring(0, gcdLength);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
+        return a;
         
-        int high = smaller.length();
-        int sLen = high;
-        int lLen = larger.length();
+//         if (!(str1 + str2).equals(str2 + str1)) {
+//             return "";
+//         }
+//         int len1 = str1.length();
+//         int len2 = str2.length();
+//         String larger = "";
+//         String smaller = "";
         
-        while (high > 0) {
-            String divisor = str2.substring(0, high);
-            int divLen = divisor.length();
+//         if (len1 == len2) {
+//             if (str1.equals(str2)) {
+//                 return str1;
+//             } else {
+//                 return "";
+//             }
+//         } else if (len1 > len2) {
+//             larger = str1;
+//             smaller = str2;
+//         } else {
+//             larger = str2;
+//             smaller = str1;
+//         }
+        
+//         int high = smaller.length();
+//         int sLen = high;
+//         int lLen = larger.length();
+        
+//         while (high > 0) {
+//             String divisor = str2.substring(0, high);
+//             int divLen = divisor.length();
             
-            if (lLen % divLen != 0 || sLen % divLen != 0) {
-                high--;
-                continue;
-            }
+//             if (lLen % divLen != 0 || sLen % divLen != 0) {
+//                 high--;
+//                 continue;
+//             }
             
-            for (int i = 0; i < lLen; i++) {
-                if ((larger.charAt(i) != divisor.charAt(i%divLen))
-                    || (smaller.charAt(i%sLen) != divisor.charAt(i%divLen))) {
-                    break;
-                }
-                if (i == lLen - 1) {
-                    return divisor;
-                }
-            }
-            high--;
-        }
-        return "";
+//             for (int i = 0; i < lLen; i++) {
+//                 if ((larger.charAt(i) != divisor.charAt(i%divLen))
+//                     || (smaller.charAt(i%sLen) != divisor.charAt(i%divLen))) {
+//                     break;
+//                 }
+//                 if (i == lLen - 1) {
+//                     return divisor;
+//                 }
+//             }
+//             high--;
+//         }
+//         return "";
     }
 }
