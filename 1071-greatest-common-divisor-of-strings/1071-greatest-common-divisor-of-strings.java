b780d700623abe3be7c8d60a7054035edc4de067
Usman Bin Arshad <hoxiusman@gmail.com>
2024-09-18 17:45:11 +0500

    Time: 4 ms (12.46%), Space: 44.4 MB (11.19%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        String larger = "";
+        String smaller = "";
+        
+        if (len1 == len2) {
+            if (str1.equals(str2)) {
+                return str1;
+            } else {
+                return "";
+            }
+        } else if (len1 > len2) {
+            larger = str1;
+            smaller = str2;
+        } else {
+            larger = str2;
+            smaller = str1;
+        }
+        
+        int high = smaller.length();
+        int sLen = high;
+        int lLen = larger.length();
+        
+        while (high > 0) {
+            String divisor = str2.substring(0, high);
+            int divLen = divisor.length();
+            
+            if (lLen % divLen != 0 || sLen % divLen != 0) {
+                high--;
+                continue;
+            }
+            
+            for (int i = 0; i < lLen; i++) {
+                if ((larger.charAt(i) != divisor.charAt(i%divLen))
+                    || (smaller.charAt(i%sLen) != divisor.charAt(i%divLen))) {
+                    break;
+                }
+                if (i == lLen - 1) {
+                    return divisor;
+                }
+            }
+            high--;
+        }
+        return "";
+    }
+}
