637e7d6a87f2825bc948cdd4e29d6a9b8c00dcc9
Usman Bin Arshad <hoxiusman@gmail.com>
2024-09-18 23:33:26 +0500

    Time: 3 ms (80.23%), Space: 45 MB (49.49%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,30 +1,54 @@
 class Solution {
     public String reverseVowels(String s) {
-        int len = s.length();
-        List<Character> vowelList = new ArrayList<>();
         Set<Character> vowelSet = new HashSet<>();
         Collections.addAll(vowelSet, 'A', 'E', 'I', 'O', 'U',
                            'a', 'e', 'i', 'o', 'u');
         
-        for (int i = 0; i < len; i++) {
-            char curr = s.charAt(i);
-            if (vowelSet.contains(curr)) {
-                vowelList.add(curr);
+        
+        char[] charString = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            while (start < end && !vowelSet.contains(charString[start])) {
+                start ++;
             }
+            while (start < end && !vowelSet.contains(charString[end])) {
+                end --;
+            }
+            char temp = charString[start];
+            charString[start] = charString[end];
+            charString[end] = temp;
+            
+            start++;
+            end--;
         }
         
-        int vowelPointer = vowelList.size() - 1;
-        String result = "";
-        
-        for (int i = 0; i < len; i++) {
-            char curr = s.charAt(i);
-            if (vowelSet.contains(curr)) {
-                result += vowelList.get(vowelPointer);
-                vowelPointer--;
-            } else {
-                result += curr;
-            }
-        }
+        String result = new String(charString);
         return result;
+        
+        
+//         int len = s.length();
+//         List<Character> vowelList = new ArrayList<>();
+        
+//         for (int i = 0; i < len; i++) {
+//             char curr = s.charAt(i);
+//             if (vowelSet.contains(curr)) {
+//                 vowelList.add(curr);
+//             }
+//         }
+        
+//         int vowelPointer = vowelList.size() - 1;
+//         String result = "";
+        
+//         for (int i = 0; i < len; i++) {
+//             char curr = s.charAt(i);
+//             if (vowelSet.contains(curr)) {
+//                 result += vowelList.get(vowelPointer);
+//                 vowelPointer--;
+//             } else {
+//                 result += curr;
+//             }
+//         }
+//         return result;
     }
 }
