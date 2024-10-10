3fd61963a73974152e2bee6fc0b992c60777dde4
Usman Bin Arshad <hoxiusman@gmail.com>
2024-10-10 14:41:26 +0500

    Time: 2 ms (95.17%), Space: 46.1 MB (98.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,21 +3,25 @@ class Solution {
         int[] res = new int[n+1];
         res[0] = 0;
         for (int i = 1; i <= n; i++) {
-            int highest = 0;
-            while (Math.pow(2, highest) < i) {
-                highest++;
-            }
-            int num = i;
-            int ones = 0;
-            while (num > 0) {
-                if (Math.pow(2,highest) <= num) {
-                    num -= Math.pow(2,highest);
-                    ones++;
-                }
-                highest--;
-            }
+            int ones = res[i>>1] + (i & 1);
             res[i] = ones;
         }
+        // for (int i = 1; i <= n; i++) {
+        //     int highest = 0;
+        //     while (Math.pow(2, highest) < i) {
+        //         highest++;
+        //     }
+        //     int num = i;
+        //     int ones = 0;
+        //     while (num > 0) {
+        //         if (Math.pow(2,highest) <= num) {
+        //             num -= Math.pow(2,highest);
+        //             ones++;
+        //         }
+        //         highest--;
+        //     }
+        //     res[i] = ones;
+        // }
         return res;
     }
 }
