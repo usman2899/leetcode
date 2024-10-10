29b8b05aada7850d31914ba27c404af1ec428970
Usman Bin Arshad <hoxiusman@gmail.com>
2024-10-10 15:04:28 +0500

    Time: 1 ms (99.80%), Space: 46.5 MB (11.05%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,25 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> res = new HashSet<>();
-        
+        //XOR
+        int res = 0;
         for (int num: nums) {
-            if (res.contains(num)) {
-                res.remove(num);
-            } else{
-                res.add(num);
-            }
+            res ^= num;
         }
-        for (int element: res) {
-            return element;
-        }
-        return 0;
+        return res;
+        
+//         //Set
+//         Set<Integer> res = new HashSet<>();
+        
+//         for (int num: nums) {
+//             if (res.contains(num)) {
+//                 res.remove(num);
+//             } else{
+//                 res.add(num);
+//             }
+//         }
+//         for (int element: res) {
+//             return element;
+//         }
+//         return 0;
     }
 }
