62d3781cacb60da9898be99a74a9ca21f74679dc
Usman Bin Arshad <hoxiusman@gmail.com>
2024-12-03 12:52:17 +0500

    Time: 11 ms (76.09%), Space: 45.8 MB (30.07%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -2,31 +2,43 @@ class Solution {
     public String predictPartyVictory(String senate) {
         Queue<Integer> rQueue = new LinkedList<>();
         Queue<Integer> dQueue = new LinkedList<>();
-        Set<Integer> voted = new HashSet<>();
+        int n = senate.length();
         
-        for (int i = 0; i < senate.length(); i++) {
+        for (int i = 0; i < n; i++) {
             if (senate.charAt(i) == 'R') {
                 rQueue.offer(i);
             } else {
                 dQueue.offer(i);
             }
         }
+        
         while (!rQueue.isEmpty() && !dQueue.isEmpty()) {
-            if (voted.contains(rQueue.peek()) && voted.contains(dQueue.peek())) {
-                voted.clear();
-            }
-            if (voted.contains(dQueue.peek()) || (rQueue.peek() < dQueue.peek() &&                          !voted.contains(rQueue.peek()))) {
-                dQueue.poll();
-                int curr = rQueue.poll();
-                voted.add(curr);
-                rQueue.offer(curr);
+            if (rQueue.peek() < dQueue.peek()) {
+                rQueue.offer(n++);
             } else {
-                rQueue.poll();
-                int curr = dQueue.poll();
-                voted.add(curr);
-                dQueue.offer(curr);
-            }  
+                dQueue.offer(n++);
+            }
+            dQueue.poll();
+            rQueue.poll();
         }
+        
+        // Set<Integer> voted = new HashSet<>();
+        // while (!rQueue.isEmpty() && !dQueue.isEmpty()) {
+        //     if (voted.contains(rQueue.peek()) && voted.contains(dQueue.peek())) {
+        //         voted.clear();
+        //     }
+        //     if (voted.contains(dQueue.peek()) || (rQueue.peek() < dQueue.peek() &&                          !voted.contains(rQueue.peek()))) {
+        //         dQueue.poll();
+        //         int curr = rQueue.poll();
+        //         voted.add(curr);
+        //         rQueue.offer(curr);
+        //     } else {
+        //         rQueue.poll();
+        //         int curr = dQueue.poll();
+        //         voted.add(curr);
+        //         dQueue.offer(curr);
+        //     }  
+        // }
         return rQueue.isEmpty() ? "Dire" : "Radiant";
     }
 }
