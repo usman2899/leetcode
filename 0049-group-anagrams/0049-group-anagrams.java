d7d20e297b9668c16c31788aa8b5ad38ebe72bcf
Usman Bin Arshad <hoxiusman@gmail.com>
2025-05-22 17:29:21 -0400

    Time: 19 ms (20.03%), Space: 49.6 MB (6.02%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+
+        for (String str: strs) {
+            int[] alphabets = new int[26];
+            for (int i = 0; i < str.length(); i++) {
+                int alphabet = str.charAt(i) - 'a';
+                alphabets[alphabet]++;
+            }
+            String alphabetFrequency = Arrays.toString(alphabets);
+            if (map.containsKey(alphabetFrequency)) {
+                List<String> group = map.get(alphabetFrequency);
+                group.add(str);
+                map.put(alphabetFrequency, group);
+            } else {
+                List<String> group = new ArrayList<>();
+                group.add(str);
+                map.put(alphabetFrequency, group);
+            }
+        }
+
+        for (String alphabetFrequency: map.keySet()) {
+            res.add(map.get(alphabetFrequency));
+        }
+        return res;
+    }
+}
