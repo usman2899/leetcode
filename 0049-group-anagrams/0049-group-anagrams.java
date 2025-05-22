6a2125082975f732d47683c2ae8bdb2c46e14e54
Usman Bin Arshad <hoxiusman@gmail.com>
2025-05-22 17:30:56 -0400

    Time: 6 ms (98.1%), Space: 48 MB (56.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,46 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        List<List<String>> res = new ArrayList<>();
+        List<List<String>> grouped = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
 
-        for (String str: strs) {
-            int[] alphabets = new int[26];
-            for (int i = 0; i < str.length(); i++) {
-                int alphabet = str.charAt(i) - 'a';
-                alphabets[alphabet]++;
-            }
-            String alphabetFrequency = Arrays.toString(alphabets);
-            if (map.containsKey(alphabetFrequency)) {
-                List<String> group = map.get(alphabetFrequency);
-                group.add(str);
-                map.put(alphabetFrequency, group);
-            } else {
-                List<String> group = new ArrayList<>();
-                group.add(str);
-                map.put(alphabetFrequency, group);
-            }
+        for(String word : strs){
+            char [] cur = word.toCharArray();
+            Arrays.sort(cur);
+            String key = new String(cur);
+            map.computeIfAbsent(key, k-> new ArrayList<>()).add(word);
         }
-
-        for (String alphabetFrequency: map.keySet()) {
-            res.add(map.get(alphabetFrequency));
-        }
-        return res;
+        grouped.addAll(map.values());
+        return grouped;
+        
     }
 }
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> map = new HashMap<>();
+//         List<List<String>> res = new ArrayList<>();
+
+//         for (String str: strs) {
+//             int[] alphabets = new int[26];
+//             for (int i = 0; i < str.length(); i++) {
+//                 int alphabet = str.charAt(i) - 'a';
+//                 alphabets[alphabet]++;
+//             }
+//             String alphabetFrequency = Arrays.toString(alphabets);
+//             if (map.containsKey(alphabetFrequency)) {
+//                 List<String> group = map.get(alphabetFrequency);
+//                 group.add(str);
+//                 map.put(alphabetFrequency, group);
+//             } else {
+//                 List<String> group = new ArrayList<>();
+//                 group.add(str);
+//                 map.put(alphabetFrequency, group);
+//             }
+//         }
+
+//         for (String alphabetFrequency: map.keySet()) {
+//             res.add(map.get(alphabetFrequency));
+//         }
+//         return res;
+//     }
+// }
