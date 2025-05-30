8d9af42d39d242789e15fa417d395f34ca93b736
Usman Bin Arshad <hoxiusman@gmail.com>
2025-05-29 21:43:56 -0400

    Time: 327 ms (5.1%), Space: 71.9 MB (5.34%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -2,25 +2,45 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> a - b);
-        for (int num: nums) {
-            minHeap.add(num);
-        }
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
+        for (int num: nums) set.add(num);
         
-        int maxConsecutive = 1;
-        int currentConsecutive = 1;
-        int prevNum = minHeap.poll();
-        while(!minHeap.isEmpty()) {
-            int currentNum = minHeap.poll();
-            if (prevNum == currentNum) continue;
-            if (prevNum + 1 == currentNum) {
-                currentConsecutive++;
-            } else {
-                maxConsecutive = Math.max(maxConsecutive, currentConsecutive);
-                currentConsecutive = 1;
+        int result = 1;
+        for (Integer num: set) {
+            if (visited.contains(num)) continue;
+            int curr = num;
+            int count = 1;
+            while(set.contains(curr+1)) {
+                count++;
+                visited.add(curr);
+                curr = curr+1;
             }
-            prevNum = currentNum;
+            visited.add(curr);
+            result = Math.max(count, result);
         }
-        return Math.max(maxConsecutive, currentConsecutive);
+        return result;
+        // if (nums.length == 0) return 0;
+
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> a - b);
+        // for (int num: nums) {
+        //     minHeap.add(num);
+        // }
+        
+        // int maxConsecutive = 1;
+        // int currentConsecutive = 1;
+        // int prevNum = minHeap.poll();
+        // while(!minHeap.isEmpty()) {
+        //     int currentNum = minHeap.poll();
+        //     if (prevNum == currentNum) continue;
+        //     if (prevNum + 1 == currentNum) {
+        //         currentConsecutive++;
+        //     } else {
+        //         maxConsecutive = Math.max(maxConsecutive, currentConsecutive);
+        //         currentConsecutive = 1;
+        //     }
+        //     prevNum = currentNum;
+        // }
+        // return Math.max(maxConsecutive, currentConsecutive);
     }
 }
