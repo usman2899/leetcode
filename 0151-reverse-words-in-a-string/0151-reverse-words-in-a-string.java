4350949b83da6cf97774d1339b421c243848472c
Usman Bin Arshad <hoxiusman@gmail.com>
2025-05-30 11:59:01 -0400

    Time: 4 ms (94.79%), Space: 43.1 MB (70.64%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,21 +1,55 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
-        s = s + " ";
-        boolean isW = true;
-        int start = 0;
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (!isW && s.charAt(i) != ' ') {
-                start = i;
-                isW = true;
-            } else if (isW && s.charAt(i) == ' ') {
-                result =  s.substring(start, i) + " " + result;
-                isW = false;
+        int len = s.length();
+        int i = 0;
+        Stack<String> stack = new Stack<>();
+        while(i < len) {
+            if (s.charAt(i) == ' ') {
+                i++;
             } else {
+                int j = i;
+                while(j < len && s.charAt(j) != ' ') {
+                    j++;
+                }
+                String word = s.substring(i,j);
+                stack.push(word);
+                i = j;
             }
         }
-        result = result.substring(0, result.length()-1);
-        return result;
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()) {
+            result.append(stack.pop());
+            result.append(" ");
+        }
+        return result.toString().trim();
     }
 }
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] words = s.split(" ");
+//         int left = 0;
+//         int right = words.length - 1;
+
+//         // while (left < right) {
+//         //     String temp = words[left];
+//         //     words[left] = words[right];
+//         //     words[right] = temp;
+//         //     left++;
+//         //     right--;
+//         // }
+
+//         StringBuilder result = new StringBuilder();
+//         // String result = "";
+//         for (int i = words.length - 1 ; i>=0; i--) {
+//             if (!words[i].isEmpty()) {
+//                 result.append(words[i]);
+//                 result.append(" ");
+//                 // result += words[i];
+//                 // result += " ";
+//             }
+//         }
+
+//         return result.toString().trim();
+//     }
+// }
