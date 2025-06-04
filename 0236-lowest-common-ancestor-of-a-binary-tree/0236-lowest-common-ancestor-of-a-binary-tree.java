5111c29268739f25c91a0f3ed9a3c1844e42f940
Usman Bin Arshad <hoxiusman@gmail.com>
2025-06-04 18:06:41 -0400

    Time: 7 ms (64.92%), Space: 44.9 MB (36.51%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,46 +9,13 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Stack<TreeNode> pStack = new Stack<>();
-        Stack<TreeNode> qStack = new Stack<>();
+        if (root == null || root == p || root == q) return root;
 
-        findPath(root, p, pStack);
-        findPath(root, q, qStack);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        Collections.reverse(pStack);
-        Collections.reverse(qStack);
-
-        TreeNode lca = null;
-        
-        while (!pStack.isEmpty() && !qStack.isEmpty()) {
-            TreeNode pNode = pStack.pop();
-            TreeNode qNode = qStack.pop();
-
-            if (pNode == qNode) {
-                lca = pNode;
-            } else {
-                break;
-            }
-        }
-        return lca;
-    }
-
-    private boolean findPath(TreeNode root, TreeNode target, Stack<TreeNode> stack) {
-        if (root == null) {
-            return false;
-        }
-
-        stack.push(root);
-
-        if (root == target) {
-            return true;
-        }
-
-        if (findPath(root.left, target, stack) || findPath(root.right, target, stack)) {
-            return true;
-        }
-
-        stack.pop();
-        return false;
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
     }
 }
