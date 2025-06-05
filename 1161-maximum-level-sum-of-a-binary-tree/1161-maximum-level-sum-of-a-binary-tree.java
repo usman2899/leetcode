40120c0ec794a0685ebc84cc165cc4dc4e4224b9
Usman Bin Arshad <hoxiusman@gmail.com>
2025-06-05 00:24:41 -0400

    Time: 9 ms (38.49%), Space: 46.9 MB (89.1%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -14,34 +14,59 @@
  * }
  */
 class Solution {
+    
+    Map<Integer, Integer> map = new HashMap<>();
     public int maxLevelSum(TreeNode root) {
+        dfs(root, 1);
+
         int maxSum = Integer.MIN_VALUE;
-        int result = 0;
-        int level = 0;
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        
-        while(!queue.isEmpty()) {
-            int sum = 0;
-            int levelSize = queue.size();
-            level++;
-            
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode curr = queue.poll();
-                sum += curr.val;
-                if (curr.left != null) {
-                    queue.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    queue.offer(curr.right);
-                }
-            }
-            if (sum > maxSum) {
-                maxSum = sum;
-                result = level;
+        int res = Integer.MAX_VALUE;
+
+        for(Integer key: map.keySet()) {
+            if (map.get(key) > maxSum || (map.get(key) == maxSum && key < res)) {
+                res = key;
+                maxSum = map.get(key);
             }
         }
-        return result;
+        return res;
+        
+    }
+    void dfs(TreeNode root, int level) {
+        if (root == null) return;
+
+        map.put(level, map.getOrDefault(level, 0) + root.val);
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
     }
 }
+
+// class Solution {
+//     List<Integer> list = new ArrayList<>();
+
+//     public int maxLevelSum(TreeNode root) {
+//         dfs(root, 0);
+//         int res = 0;
+//         int max = Integer.MIN_VALUE;
+//         for (int i = 0; i < list.size(); i++) {
+//             if (list.get(i) > max) {
+//                 max = list.get(i);
+//                 res = i;
+//             }
+//         }
+//         return res+1;
+//     }
+
+//     void dfs(TreeNode root, int level) {
+//         if (root == null) {
+//             return;
+//         }
+//         if (list.size() == level) {
+//             list.add(root.val);
+//         } else {
+//             int newVal = list.get(level) + root.val;
+//             list.set(level, newVal);
+//         }
+//         dfs(root.left, level+1);
+//         dfs(root.right, level+1);
+//     }
+// }
