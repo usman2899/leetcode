56e18448a8b5954021671e0ec855ed732d5a6154
Usman Bin Arshad <hoxiusman@gmail.com>
2025-06-08 12:04:38 -0400

    Time: 1 ms (100%), Space: 44.5 MB (54.01%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,50 +10,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        ListNode ptr = head;
-        boolean carry = false;
-        while (l1 != null || l2 != null) {
-            ListNode curr = new ListNode();
-            if (l1 == null) {
-                if (carry) {
-                    int value = l2.val + 1;
-                    curr.val = value % 10;
-                    if (value < 10) carry = false;
-                } else {
-                    curr.val = l2.val;
-                }
-                l2 = l2.next;
-            } else if (l2 == null) {
-                if (carry) {
-                    int value = l1.val + 1;
-                    curr.val = value % 10;
-                    if (value < 10) carry = false;
-                } else {
-                    curr.val = l1.val;
-                }
-                l1 = l1.next;
+        int carry = 0;
+        ListNode resNode = new ListNode();
+        ListNode curr = resNode;
+
+        while(l1 != null || l2 != null || carry != 0) {
+            int num1 = l1 == null ? 0 : l1.val;
+            int num2 = l2 == null ? 0 : l2.val;
+
+            int resVal = num1 + num2 + carry;
+            if (resVal > 9) {
+                carry = 1;
+                resVal = resVal % 10;
             } else {
-                if (carry) {
-                    int value = l1.val + l2.val + 1;
-                    curr.val = value % 10;
-                    if (value < 10) carry = false;
-                } else {
-                    int value = l1.val + l2.val;
-                    curr.val = value % 10;
-                    if (value > 9) carry = true;
-                }
-                l1 = l1.next;
-                l2 = l2.next;
+                carry = 0;
             }
-            ptr.next = curr;
-            ptr = ptr.next;
+
+            ListNode temp = new ListNode(resVal);
+            curr.next = temp;
+            curr = curr.next;
+            l1 = l1 == null ? l1 : l1.next;
+            l2 = l2 == null ? l2 : l2.next;
         }
-        if (carry) {
-            ListNode curr = new ListNode(1);
-            ptr.next = curr;
-            ptr = ptr.next;
-        }
-        return head.next;
+        return resNode.next;
     }
 }
