adba34472e4d92c913c44a363f5ddab4cd2f0baf
Usman Bin Arshad <hoxiusman@gmail.com>
2025-06-08 21:26:21 -0400

    Time: 1 ms (67.88%), Space: 41.5 MB (80.86%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,33 +1,28 @@
 class Solution {
-    public String decodeString(String s) {
-        Stack<Pair<Integer, StringBuilder>> stack = new Stack<>();
-        
-        StringBuilder curr = new StringBuilder();
-        int num = 0; 
-        
-        for (char currChar: s.toCharArray()) {            
-            if (Character.isDigit(currChar)) {
-                num = num * 10 + (Character.getNumericValue(currChar));
-            }
-            
-            else if (currChar == '[') {
-                stack.push(new Pair<>(num, curr));
-                num = 0;
-                curr = new StringBuilder();;
-            }
-            
-            else if (currChar == ']') {
-                Pair<Integer, StringBuilder> pair = stack.pop();
-                StringBuilder temp = pair.getValue();
-                temp.append(curr.toString().repeat(pair.getKey()));
-                curr = temp;
-            }
-            
-            else {
-                curr.append(currChar);
-            }
-        }
-        
-        return curr.toString();
-    }
+   public String decodeString(String s) {
+       Stack<Pair<Integer, StringBuilder>> stack = new Stack<>();       
+       StringBuilder curr = new StringBuilder();
+       StringBuilder num = new StringBuilder();
+      
+       for (char currChar: s.toCharArray()) {           
+           if (Character.isDigit(currChar)) {
+               num.append(currChar);
+           }           
+           else if (currChar == '[') {
+               stack.push(new Pair<>(Integer.valueOf(num.toString()), curr));
+               num = new StringBuilder();
+               curr = new StringBuilder();
+           }
+           else if (currChar == ']') {
+               Pair<Integer, StringBuilder> pair = stack.pop();
+               StringBuilder temp = pair.getValue();
+               temp.append(curr.toString().repeat(pair.getKey()));
+               curr = temp;
+           }
+           else {
+               curr.append(currChar);
+           }
+       }
+       return curr.toString();
+   }
 }
